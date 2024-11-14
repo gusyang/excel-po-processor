91052ccabaa75c55035a52948b9941e2ee2f8729
Gus Yang <gusyang@users.noreply.github.com>
2024-11-14 12:20:00 -0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
+import { CONFIG } from './config.js';
+
+export const debug = {
+  log: (...args) => CONFIG.DEBUG && console.log('[DEBUG]', ...args),
+  error: (...args) => CONFIG.DEBUG && console.error('[DEBUG]', ...args)
+};
+
 export function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 } 
