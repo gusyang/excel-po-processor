5c303fd9509c3c1138e1babad788189f0e9eb110
Gus Yang <gusyang@users.noreply.github.com>
2025-01-03 13:01:59 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
   LOG_DIR: './logs',
   MAX_LOG_SIZE: 10 * 1024 * 1024, // 10MB
   LOG_RETENTION_DAYS: 30,
-  API_KEY: '70AB4F85-FC84-48C2-B6FB-732690694F10',
+  API_KEY: '**',
   API_COOKIE: 'ARRAffinity=cfb98572471b0c41299b8ff4c92bbd3ef0be3e4e54d0a694aa6258a51d038669; ARRAffinitySameSite=cfb98572471b0c41299b8ff4c92bbd3ef0be3e4e54d0a694aa6258a51d038669',
   API_URL: 'https://importanalyst.azurewebsites.net/api/v2/FTZ/GetPurchaseOrderDetails',
   DEBUG: true,
